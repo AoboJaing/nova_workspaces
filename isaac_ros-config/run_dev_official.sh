4f9a7c6dca82ba825b2fa9f868f0f1d274b589b6
Aobo Jaing <744910955@qq.com>
2025-08-18 14:16:40 +0800

    chore: init repo with base .gitignore and add isaac_ros-config/run_dev_official.sh

---
+++ isaac_ros-config/run_dev_official.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# 当前目录作为工作区根目录
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ISAAC_WS_PATH="${SCRIPT_DIR}/isaac_ros-dev/"
+
+# 临时环境变量写在命令前面，只作用于 run_dev.sh 这一条命令
+ISAAC_ROS_WS="$ISAAC_WS_PATH" CONFIG_CONTAINER_NAME_SUFFIX=official \
+    "${ISAAC_WS_PATH}/src/isaac_ros_common/scripts/run_dev.sh" -d "$ISAAC_WS_PATH" "$@"

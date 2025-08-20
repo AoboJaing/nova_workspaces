5a1b66f73213ff5c5b1df3ba939a09eab59cbce3
Aobo Jaing <744910955@qq.com>
2025-08-20 17:32:07 +0800

    由于run_dev_official.sh文件的路径变了，所以对应在这个文件里面的相关路径也要修改

--- isaac_ros-config/run_dev_official.sh
+++ isaac_ros-config/run_dev_official.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # 当前目录作为工作区根目录
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ISAAC_WS_PATH="${SCRIPT_DIR}/isaac_ros-dev/"
+ISAAC_WS_PATH="${SCRIPT_DIR}/../isaac_ros-dev/"
 
 # 临时环境变量写在命令前面，只作用于 run_dev.sh 这一条命令
 ISAAC_ROS_WS="$ISAAC_WS_PATH" CONFIG_CONTAINER_NAME_SUFFIX=official \
